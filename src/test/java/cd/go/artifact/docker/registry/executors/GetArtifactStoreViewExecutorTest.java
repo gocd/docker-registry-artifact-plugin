92f921ea3a32edab360b32064b469dda919f7d80
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2019-01-06 19:36:35 -0800

    Update artifact store view.

--- src/test/java/cd/go/artifact/docker/registry/executors/GetArtifactStoreViewExecutorTest.java
+++ src/test/java/cd/go/artifact/docker/registry/executors/GetArtifactStoreViewExecutorTest.java
@@ -16,38 +16,24 @@
 
 package cd.go.artifact.docker.registry.executors;
 
-import cd.go.artifact.docker.registry.annotation.ConfigMetadata;
-import cd.go.artifact.docker.registry.annotation.MetadataHelper;
-import cd.go.artifact.docker.registry.model.ArtifactStoreConfig;
 import cd.go.artifact.docker.registry.utils.Util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GetArtifactStoreViewExecutorTest extends ViewTest {
+public class GetArtifactStoreViewExecutorTest {
     @Test
     public void shouldRenderTheTemplateInJSON() throws Exception {
-        GoPluginApiResponse response = getRequestExecutor().execute();
+        GoPluginApiResponse response = new GetArtifactStoreViewExecutor().execute();
 
         Map<String, String> responseHash = new Gson().fromJson(response.responseBody(), new TypeToken<Map<String,String>>(){}.getType());
 
         assertThat(response.responseCode()).isEqualTo(200);
         assertThat(responseHash).containsEntry("template", Util.readResource("/artifact-store.template.html"));
-    }
-
-    @Override
-    protected List<ConfigMetadata> getMetadataList() {
-        return MetadataHelper.getMetadata(ArtifactStoreConfig.class);
-    }
-
-    @Override
-    protected RequestExecutor getRequestExecutor() {
-        return new GetArtifactStoreViewExecutor();
     }
 }
